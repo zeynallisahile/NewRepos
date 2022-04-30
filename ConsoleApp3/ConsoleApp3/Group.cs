28c907dfa2062834a69fe4fe9a951657857bf978
zeynallisahile <99805984+zeynallisahile@users.noreply.github.com>
2022-04-29 21:46:37 -0700

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Group.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Group
+    {
+        public Group(string groupNo, int studentLimit)
+        {
+            GroupNo = groupNo;
+            StudentLimit = studentLimit;
+        }
+        private int _studentLimit;
+        private string _groupNo;
+        public string GroupNo
+        {
+            get => _groupNo;
+            set
+            {
+                if (CheckGroupNo(value))
+                    _groupNo = value;
+            }
+        }
+        public int StudentLimit
+        {
+            get => _studentLimit;
+            set
+            {
+                if (value >= 5 && value <= 18)
+                    _studentLimit = value;
+            }
+        }
+        private Student[] students = new Student[0];
+        public bool CheckGroupNo(string groupNo)
+        {
+            if (!string.IsNullOrWhiteSpace(groupNo) && groupNo.Length == 5)
+            {
+                if (char.IsUpper(groupNo[0]) && char.IsUpper(groupNo[1]) && char.IsDigit(groupNo[2]) && char.IsDigit(groupNo[3]) && char.IsDigit(groupNo[4]))
+                    return true;
+                else
+                    return false;
+            }
+            return false;
+        }
+        public void AddStudent(Student student)
+        {
+            if (students.Length <= StudentLimit)
+            {
+                Array.Resize(ref students, students.Length + 1);
+                students[students.Length - 1] = student;
+            }
+            else
+                Console.WriteLine("Siz qrup limitini kecmisiz!!!");
+        }
+        public Student GetStudent(int? id)
+        {
+            if (Array.Exists(students, x => x.Id == id))
+            {
+                Student student = Array.Find(students, x => x.Id == id);
+                return student;
+            }
+            else
+            {
+                Console.WriteLine("Bele bir id yoxdur,zehmet olmasa duzgun axtaris edin!");
+                return null;
+            }
+        }
+        public Student[] GetAllStudents()
+        {
+            return students;
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Group group = null;
+            bool check = false;
+            Console.WriteLine("User yaratmaq ucun datalari daxil edin!");
+            do
+            {
+                Console.WriteLine("User-in adini daxil edin:");
+                string fullname = Console.ReadLine();
+
+                Console.WriteLine("User-in Email-ni daxil edin:");
+                string email = Console.ReadLine();
+
+                Console.WriteLine("User-in sifresini daxil edin(sifrede en az bir boyuk herf,bir kicik ve reqem olmalidir)");
+                string password;
+                do
+                {
+                    password = Console.ReadLine();
+                } while (!PasswordChecker(password));
+
+                User user = new User(fullname, email, password);
+                bool checkmenu = false;
+                do
+                {
+                    Console.WriteLine("\n------>>Giris menyusu<<-----");
+                    Console.WriteLine("1. User-in melumatlarini goster");
+                    Console.WriteLine("2. Yeni grup yatar");
+                    Console.WriteLine("3. Esas menyu");
+                    Console.WriteLine("0. Cixis");
+                    string choise = Console.ReadLine();
+                    switch (choise)
+                    {
+                        case "1":
+                            user.ShowInfo();
+                            break;
+                        case "2":
+                            Console.WriteLine("Grupun adini daxil edin:");
+                            string groupName;
+                            do
+                            {
+                                groupName = Console.ReadLine();
+                            } while (!CheckGroupNo(groupName));
+                            Console.WriteLine("Grupun telebe limitini daxil edin:");
+                            int limit;
+                            do
+                            {
+                                limit = Convert.ToInt32(Console.ReadLine());
+                            } while (limit <= 5 || limit >= 18);
+                            group = new Group(groupName, limit);
+                            break;
+                        case "3":
+                            bool checkMenu = true;
+                            Console.WriteLine("----->>Esas menyuya kecid etdiniz");
+                            do
+                            {
+                                Console.WriteLine("1. Butun telebeleri goster");
+                                Console.WriteLine("2. Id ile axtaris");
+                                Console.WriteLine("3. Telebe elave et");
+                                Console.WriteLine("0. Geri");
+
+                                string menu = Console.ReadLine();
+                                switch (menu)
+                                {
+                                    case "1":
+                                        foreach (var item in group.GetAllStudents())
+                                        {
+                                            Console.WriteLine(item.ShowInfo());
+                                        }
+                                        break;
+                                    case "2":
+                                        Console.WriteLine("Id-ni daxil edin:");
+                                        int id = Convert.ToInt32(Console.ReadLine());
+                                        Console.WriteLine(group.GetStudent(id).Fullname);
+                                        break;
+                                    case "3":
+                                        Console.WriteLine("Telebe datalarini daxil edin:");
+                                        Console.WriteLine("Telebenin adini daxil edin:");
+                                        string fullnames = Console.ReadLine();
+                                        Console.WriteLine("Telebenin balini daxil edin:");
+                                        double point;
+                                        do
+                                        {
+                                            point = Convert.ToDouble(Console.ReadLine());
+                                        } while (point <= 0 || point >= 100);
+                                        Student student1 = new Student(fullnames, point);
+                                        group.AddStudent(student1);
+                                        break;
+                                    case "0":
+                                        checkmenu = false;
+                                        break;
+                                    default:
+                                        Console.WriteLine("Duzgun secim edin!!!");
+                                        break;
+                                }
+                            } while (checkMenu);
+                            break;
+                        case "0":
+                            check = true;
+                            break;
+                        default:
+                            Console.WriteLine("Duzgun secim edin!!!");
+                            break;
+                    }
+                } while (!checkmenu);
+            } while (check);
+        }
+    }
+}
